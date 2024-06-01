34ddc175158f0347ca77c7834692edde535f81d6
Bruno <dupim1@hotmail.com>
2024-06-01 19:50:12 -0300

    Adicionando aulas 1, 2 e 3

---
+++ 2/main.go
@@ -0,0 +1,20 @@
+package main
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "oi"
+	e float64 = 1.2
+)
+
+func main() {
+	var a string = "X"
+	f := "teste"
+
+	println(a)
+	println(b)
+	println(c)
+	println(d)
+	println(e)
+	println(f)
+}
