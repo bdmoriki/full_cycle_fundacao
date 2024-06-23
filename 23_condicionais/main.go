27018d34b61e7a03df08ac1895addb193b0eb480
Bruno <dupim1@hotmail.com>
2024-06-22 21:34:56 -0300

    adicionando aula condicionais

---
+++ 23_condicionais/main.go
@@ -0,0 +1,36 @@
+package main
+
+func main() {
+	a := 10
+	b := 2
+	c := 3
+
+	// if a > b {
+	// 	println(a)
+	// } else {
+	// 	println(b)
+	// }
+
+	if a > b && c > a {
+		println(a)
+	} else {
+		println(b)
+	}
+
+	if a > b || c > a {
+		println(a)
+	} else {
+		println(b)
+	}
+
+	switch a {
+	case 1:
+		println("a")
+	case 2:
+		println("b")
+	case 3:
+		println("c")
+	default:
+		println("d")
+	}
+}
